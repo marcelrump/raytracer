a16e217bd970010721f21f156473408a95a6f19a
Marcel Rump <rumpmarcel@gmail.com>
2020-05-04 00:08:42 +0200

    Useful mathematical functions and variables

---
+++ src/math.h
@@ -0,0 +1,13 @@
+// Author: Marcel Rump
+// E-Mail: rumpmarcel@gmail.com
+
+#ifndef RAYTRACER_SRC_MATH_H_
+#define RAYTRACER_SRC_MATH_H_
+
+#include <limits>
+
+#define PI  3.1415926535979846f
+#define EPS 1e-10
+#define INF std::numeric_limits<double>::infinity()
+
+#endif // RAYTRACER_SRC_MATH_H_
