4a79ba623d2f3e75434f736c5f7f441767574726
Marcel Rump <rumpmarcel@gmail.com>
2020-05-03 23:53:28 +0200

    Definition of global function 'int main()'

---
+++ src/raytracer.cpp
@@ -0,0 +1,18 @@
+// Author: Marcel Rump
+// E-Mail: rumpmarcel@gmail.com
+
+// C system headers
+// (more precisely: headers in angle brackets with the .h extension), e.g. <unistd.h>, <stdlib.h>.
+
+// C++ standard library headers
+// (without file extension), e.g. <algorithm>, <cstddef>.
+
+// Other libraries' headers
+
+// Project headers
+
+// int main(int argc, const char * argv[])
+int main()
+{
+  
+}
